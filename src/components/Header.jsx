087e889e67da03917bce796a3eae727833346a97
Amankumar <amanks2580@gmail.com>
2024-10-09 15:17:04 +0530

    added resume downloading link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,37 +29,23 @@ const Header = () => {
             isMenuOpen ? 'block' : 'hidden'
           } absolute top-full mt-1 left-0 w-full bg-white md:bg-transparent text-center  md:relative md:flex md:w-auto md:items-center z-[1] md:z-auto`}
         >
-        
         <ul className="flex flex-col md:flex-row md:items-center space-y-6 md:space-y-0 md:space-x-8 py-6 md:py-0 w-full text-center">
         <li className="font-semibold top-full translate-y-0">
           <Link to = "Home" smooth = {true} duration={500} className ="duration-300 hover:text-blue-600 cursor-pointer">
             Home
           </Link>
-            {/* <a title="Home" href="/#home" className ="duration-300 hover:text-blue-600">
-              Home
-            </a> */}
           </li>
-        
           <li className="font-semibold top-full translate-y-0">
-            {/* <a title="About" href="/#about" className ="duration-300 hover:text-blue-600">
-              About
-            </a> */}
             <Link to = "About" smooth = {true} duration={500} className ="duration-300 hover:text-blue-600 cursor-pointer">
             About
           </Link>
           </li>
           <li className="font-semibold top-full translate-y-0">
-            {/* <a title="Projects" href="/#portfolio" className ="duration-300 hover:text-blue-600">
-              Projects
-            </a> */}
             <Link to = "Projects" smooth = {true} duration={500} className ="duration-300 hover:text-blue-600 cursor-pointer">
             Projects
           </Link>
           </li>
           <li className="font-semibold top-full translate-y-0">
-            {/* <a title="Contact" href="/#contact" className ="duration-300 hover:text-blue-600">
-              Contact
-            </a> */}
             <Link to = "Contact" smooth = {true} duration={500} className ="duration-300 hover:text-blue-600 cursor-pointer">
             Contact
           </Link>
